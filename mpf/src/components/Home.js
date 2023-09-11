a226dc4963562a1861fc283c6fb860ab71eb13fd
Peggy <peggykimotho@gmail.com>
2023-09-11 13:08:05 +0000

    Styled footer

--- mpf/src/components/Home.js
+++ mpf/src/components/Home.js
@@ -4,7 +4,7 @@ function Home(){
     return(
         <div className="home-pg" id="home">
            <h1>Hi, I'm Peggy.</h1> 
-           <h2>A Full-Stack Developer.</h2> 
+           <h2>A Full-Stack Software Developer.</h2> 
            <p className="p">Certified in building innovative and impactful web applications. My expertise lies in utilizing HTML, CSS, JavaScript, React, Bootstrap, Python and Flask to bring ideas to life.</p>
             <div className="home-btn">
             <a href="Peggy Kimotho Resume.pdf" target="_blank" rel="noreferrer"><button>View Resume</button></a>
